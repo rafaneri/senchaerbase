b538945a5ae18aa1a358acc5a18c501fa9a786c0
Rafael Neri <rafael.neri@gmail.com>
2015-04-16 13:24:13 -0300

    PhoneGap app

---
+++ src/apperbase/www/app/app.js
@@ -0,0 +1,22 @@
+/**
+* Define o modelo para o objeto Grupo
+*/
+Ext.Loader.setConfig({
+    paths: {
+        Ext: '../js'
+    }
+});
+Ext.application({
+
+    name: 'NotasErbase',
+    requires: [],
+    controllers: [],
+    views: [],
+
+    launch: function() {
+
+        
+
+    }
+
+});
